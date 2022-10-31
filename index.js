919e842590b67161ce7a4a42fbf1c0bd9119cafd
phongngotrong <ngotrongphong.dev@gmail.com>
2022-10-31 07:51:48 +0700

    setup

--- index.js
+++ index.js
@@ -9,3 +9,20 @@ app.use(
     methods: ["GET", "POST", "PUT", "DELETE"],
   })
 );
+
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
+app.use("/", (req, res) => {
+  return res.send("Sever on...");
+});
+
+const PORT = process.env.PORT || 8888;
+
+const listener = app.listen(PORT, () => {
+  console.log("Server is running on port " + listener.address().port);
+});
